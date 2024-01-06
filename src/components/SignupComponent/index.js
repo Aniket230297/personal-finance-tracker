f0168e817eb6e7a02e80ebe209d57ab9786dcae8
Aniket <aniketkule231997@gmail.com>
2024-01-06 18:54:20 +0530

    jn

--- src/components/SignupComponent/index.js
+++ src/components/SignupComponent/index.js
@@ -3,9 +3,10 @@ import "./style.css";
 import Input from '../Input';
 import { useState } from 'react';
 import Button from '../Button';
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from '../Firebase';
 import { toast } from 'react-toastify';
+import { useNavigate } from 'react-router-dom';
 
 
 function SignupComponent() {
@@ -14,6 +15,8 @@ function SignupComponent() {
   const [password, setPassword] = useState("");
   const [confirmpassword, setConfirmpassword] = useState("");
   const [loading, setLoading]=useState(false);
+  const [loginform, setLoginform]=useState(false);
+  const navigate= useNavigate();
 
   function SignupUsingEmail() {
     if (name !== ""&& email !== ""&& password !== ""&& confirmpassword !== "" ){
@@ -25,6 +28,7 @@ function SignupComponent() {
           const user = userCredential.user;
           console.log(user);
           toast.success("User Created Successfully!");
+          navigate("/dashboard");
           setLoading(false);
           setName("");
           setEmail("");
@@ -49,8 +53,61 @@ function SignupComponent() {
     }
   }
 
+  function LoginUsingEmail(){
+    if ( email !== ""&& password !== "" ){
+    console.log("email" , email);
+    console.log("password", password);
+    setLoading(true);
+    signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    toast.success("Signin sucessfully!")
+    setLoading(false);
+    navigate("/dashboard");
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    toast.error(errorMessage);
+    setLoading(false);
+  })
+
+  }else{
+    toast.error("All Fields Are Mandatory!");
+    setLoading(false);
+  }
+
+  }
 
   return (
+    <>
+    {(loginform)?(<div className='signupcomponent'>
+    <p className='logotitle'>Login on <span style={{ color: 'blue' }}>Financely.</span></p>
+    <form>
+   
+    <Input text={"Email"}
+      type="email"
+      value={email}
+      setValue={setEmail}
+      placeholder={"Email"} />
+
+    <Input text={"Password"}
+      type="password"
+      value={password}
+      setValue={setPassword}
+      placeholder={"password"} />
+
+    <Button text={"Login using Email and Password"} 
+            onClick={LoginUsingEmail}
+              disabled={loading} />
+    <p className='ortext'>or</p>
+    <Button text={"Login using Google"} btnblue={true} />
+    <p className='p-tag' onClick={()=>{setLoginform(!loginform)}}>or Dont Have an Account? Click Here!</p>
+    </form>
+  </div>
+  ):(
     <div className='signupcomponent'>
       <p className='logotitle'>Sign Up on <span style={{ color: 'blue' }}>Financely.</span></p>
       <form>
@@ -83,9 +140,13 @@ function SignupComponent() {
       disabled={loading} />
       <p className='ortext'>or</p>
       <Button text={"Sign up using Google"} btnblue={true} />
+      <p className='p-tag' onClick={()=>{setLoginform(!loginform)}}>or Have an Account Already? Click Here!</p>
       </form>
     </div>
+    )
+   } </>
   )
 }
+
 
 export default SignupComponent;
