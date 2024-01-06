45ee8118d83100bf376a4de0a23ad6ba69cb544e
Aniket <aniketkule231997@gmail.com>
2024-01-06 19:45:30 +0530

    new

--- src/components/SignupComponent/index.js
+++ src/components/SignupComponent/index.js
@@ -7,6 +7,9 @@ import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "fire
 import { auth } from '../Firebase';
 import { toast } from 'react-toastify';
 import { useNavigate } from 'react-router-dom';
+import { setDoc } from '../Firebase';
+import { doc } from '../Firebase';
+import { db } from '../Firebase';
 
 
 function SignupComponent() {
@@ -14,137 +17,164 @@ function SignupComponent() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmpassword, setConfirmpassword] = useState("");
-  const [loading, setLoading]=useState(false);
-  const [loginform, setLoginform]=useState(false);
-  const navigate= useNavigate();
+  const [loading, setLoading] = useState(false);
+  const [loginform, setLoginform] = useState(false);
+  const navigate = useNavigate();
 
   function SignupUsingEmail() {
-    if (name !== ""&& email !== ""&& password !== ""&& confirmpassword !== "" ){
-      if(password === confirmpassword){
-          setLoading(true);
-          createUserWithEmailAndPassword(auth, email, password)
-        .then((userCredential) => {
-          // Signed up 
-          const user = userCredential.user;
-          console.log(user);
-          toast.success("User Created Successfully!");
-          navigate("/dashboard");
-          setLoading(false);
-          setName("");
-          setEmail("");
-          setPassword("");
-          setConfirmpassword("");
-          
-          // ...
-      })
-        .catch((error) => {
-          const errorCode = error.code;
-          console.log(errorCode);
-          const errorMessage = error.message;
-          toast.error(errorMessage);
-          setLoading(false);
-          // ..
-        })
-      }else{toast.error("Password and ConfirmPassword will be same!")
-      setLoading(false);}
-    }else{
+    if (name !== "" && email !== "" && password !== "" && confirmpassword !== "") {
+      if (password === confirmpassword) {
+        setLoading(true);
+        createUserWithEmailAndPassword(auth, email, password)
+          .then((userCredential) => {
+            // Signed up 
+            const user = userCredential.user;
+            console.log(user);
+            toast.success("User Created Successfully!");
+            createDoc(user);
+            navigate("/dashboard");
+            setLoading(false);
+            setName("");
+            setEmail("");
+            setPassword("");
+            setConfirmpassword("");
+
+            // ...
+          })
+          .catch((error) => {
+            const errorCode = error.code;
+            console.log(errorCode);
+            const errorMessage = error.message;
+            toast.error(errorMessage);
+            setLoading(false);
+            // ..
+          })
+      } else {
+        toast.error("Password and ConfirmPassword will be same!")
+        setLoading(false);
+      }
+    } else {
       toast.error("All Fields are Mandatory!");
       setLoading(false);
     }
   }
 
-  function LoginUsingEmail(){
-    if ( email !== ""&& password !== "" ){
-    console.log("email" , email);
-    console.log("password", password);
-    setLoading(true);
-    signInWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
-    // Signed in 
-    const user = userCredential.user;
-    toast.success("Signin sucessfully!")
-    setLoading(false);
-    navigate("/dashboard");
-    // ...
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    toast.error(errorMessage);
-    setLoading(false);
-  })
+  async function createDoc(user) {
+    if (!user) return;
+    const userRef = doc(db, 'users', user.uid);
+    const userData= await getDoc(userRef);
+    
+    if(!userData.exists()){
 
-  }else{
-    toast.error("All Fields Are Mandatory!");
-    setLoading(false);
+    try {
+      await setDoc(doc(db, "users", user.uid), {
+        name: user.name,
+        email: user.email,
+        Photourl: "",
+        createdAt: new Date()
+      }
+      )
+    } catch (e) {
+      toast.error(e.message);
+    }
+
+  }else(
+    toast.error("User Already Exist")
+  )
   }
+
+  function LoginUsingEmail() {
+    if (email !== "" && password !== "") {
+      console.log("email", email);
+      console.log("password", password);
+      setLoading(true);
+      signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+          // Signed in 
+          const user = userCredential.user;
+          toast.success("Signin sucessfully!")
+          setLoading(false);
+          navigate("/dashboard");
+          // ...
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          toast.error(errorMessage);
+          setLoading(false);
+        })
+
+    } else {
+      toast.error("All Fields Are Mandatory!");
+      setLoading(false);
+    }
 
   }
 
   return (
     <>
-    {(loginform)?(<div className='signupcomponent'>
-    <p className='logotitle'>Login on <span style={{ color: 'blue' }}>Financely.</span></p>
-    <form>
-   
-    <Input text={"Email"}
-      type="email"
-      value={email}
-      setValue={setEmail}
-      placeholder={"Email"} />
+      {(loginform) ? (<div className='signupcomponent'>
+        <p className='logotitle'>Login on <span style={{ color: 'blue' }}>Financely.</span></p>
+        <form>
 
-    <Input text={"Password"}
-      type="password"
-      value={password}
-      setValue={setPassword}
-      placeholder={"password"} />
+          <Input text={"Email"}
+            type="email"
+            value={email}
+            setValue={setEmail}
+            placeholder={"Email"} />
 
-    <Button text={"Login using Email and Password"} 
+          <Input text={"Password"}
+            type="password"
+            value={password}
+            setValue={setPassword}
+            placeholder={"password"} />
+
+          <Button text={"Login using Email and Password"}
             onClick={LoginUsingEmail}
+            disabled={loading} />
+          <p className='ortext'>or</p>
+          <Button text={"Login using Google"} btnblue={true} />
+          <p className='p-tag' onClick={() => { setLoginform(!loginform) }}>or Dont Have an Account? Click Here!</p>
+        </form>
+      </div>
+      ) : (
+        <div className='signupcomponent'>
+          <p className='logotitle'>Sign Up on <span style={{ color: 'blue' }}>Financely.</span></p>
+          <form>
+            <Input text={"Full Name"}
+              type="text"
+              value={name}
+              setValue={setName}
+              placeholder={"Joe Doe"} />
+
+            <Input text={"Email"}
+              type="email"
+              value={email}
+              setValue={setEmail}
+              placeholder={"Email"} />
+
+            <Input text={"Password"}
+              type="password"
+              value={password}
+              setValue={setPassword}
+              placeholder={"password"} />
+
+            <Input text={"Confirm Password"}
+              type="password"
+              value={confirmpassword}
+              setValue={setConfirmpassword}
+              placeholder={"Confirm Password"} />
+
+            <Button text={"Sign up using Email and Password"}
+              onClick={SignupUsingEmail}
               disabled={loading} />
-    <p className='ortext'>or</p>
-    <Button text={"Login using Google"} btnblue={true} />
-    <p className='p-tag' onClick={()=>{setLoginform(!loginform)}}>or Dont Have an Account? Click Here!</p>
-    </form>
-  </div>
-  ):(
-    <div className='signupcomponent'>
-      <p className='logotitle'>Sign Up on <span style={{ color: 'blue' }}>Financely.</span></p>
-      <form>
-      <Input text={"Full Name"}
-        type="text"
-        value={name}
-        setValue={setName}
-        placeholder={"Joe Doe"} />
-
-      <Input text={"Email"}
-        type="email"
-        value={email}
-        setValue={setEmail}
-        placeholder={"Email"} />
-
-      <Input text={"Password"}
-        type="password"
-        value={password}
-        setValue={setPassword}
-        placeholder={"password"} />
-
-      <Input text={"Confirm Password"}
-        type="password"
-        value={confirmpassword}
-        setValue={setConfirmpassword}
-        placeholder={"Confirm Password"} />
-
-      <Button text={"Sign up using Email and Password"} 
-      onClick={SignupUsingEmail}
-      disabled={loading} />
-      <p className='ortext'>or</p>
-      <Button text={"Sign up using Google"} btnblue={true} />
-      <p className='p-tag' onClick={()=>{setLoginform(!loginform)}}>or Have an Account Already? Click Here!</p>
-      </form>
-    </div>
-    )
-   } </>
+            <p className='ortext'>or</p>
+            <Button text={"Sign up using Google"} btnblue={true} />
+            <p className='p-tag' onClick={() => { setLoginform(!loginform) }}>or Have an Account Already? Click Here!</p>
+          </form>
+        </div>
+      )
+      } </>
   )
 }
 
