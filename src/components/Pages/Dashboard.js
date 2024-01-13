4ec06be75b62764d76d39493de21b7b78daa159c
Aniket <aniketkule231997@gmail.com>
2024-01-13 15:15:29 +0530

    new

--- src/components/Pages/Dashboard.js
+++ src/components/Pages/Dashboard.js
@@ -1,54 +1,72 @@
-import React, { useEffect , useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import Header from '../Header'
 import Cards from '../Dashboard/Card'
 import AddExpenses from '../Dashboard/Modal/AddExpenses';
 import AddIncome from '../Dashboard/Modal/AddIncome';
+import {collection } from 'firebase/firestore';
+import { toast } from 'react-toastify';
+import moment from 'moment';
+import { db } from '../Firebase';
+import { addDoc } from 'firebase/firestore';
+
 
 
 
 function Dashboard() {
   const [isExpenseModalVisible, setIsExpenseModalVisible] = useState(false);
   const [isIncomeModalVisible, setIsIncomeModalVisible] = useState(false);
-  
+  const {user} = getAuthState(auth)
 
-const showIncomeModal=()=>{
+  const showIncomeModal = () => {
     setIsIncomeModalVisible(true);
-}
+  }
 
-const showExpenseModal=()=>{
-  setIsExpenseModalVisible(true);
-}
+  const showExpenseModal = () => {
+    setIsExpenseModalVisible(true);
+  }
 
-const handleIncomeModal=()=>{
-  setIsIncomeModalVisible(false);
-}
+  const handleIncomeModal = () => {
+    setIsIncomeModalVisible(false);
+  }
 
-const handleExpenesModal=()=>{
-  setIsExpenseModalVisible(false);
-}
+  const handleExpenesModal = () => {
+    setIsExpenseModalVisible(false);
+  }
 
-const onFinish=(values, type)=>{
-console.log(values)
-}
+  const onFinish = (values, type) => {
+    const newTransaction = {
+        type:type,
+        amount:parseFloat(values.amount),
+        name:values.name,
+        date:moment(values.date).format("YYYY.MM.DD"),
+        tag:values.tag
+    };
+
+    addtransaction(newTransaction);
+  }
+
+  async function addtransaction(transaction){
+try{
+    const DocRef = await addDoc(
+      collection(db, `users/${user.uid}/transaction`),
+      transaction    
+      );
+      toast.success("Transaction added!");
+    }catch(e){
+      toast.error("Couldnt  add transaction!");
+    }
+  }
 
 
-  
- 
 
-  // console.log("income amount", income)
-  // console.log("expense amount", expense)
-  // console.log(transaction.amount)
-
- 
-    
   return (
     <div>
       <Header />
       <Cards showIncomeModal={showIncomeModal} showExpenseModal={showExpenseModal}
-       />
-      <AddIncome isIncomeModalVisible={isIncomeModalVisible} handleIncomeModal={handleIncomeModal}  onFinish={onFinish}/>
-      <AddExpenses isExpenseModalVisible={isExpenseModalVisible} handleExpenesModal={handleExpenesModal} onFinish={onFinish}/>
-    
+      />
+      <AddIncome isIncomeModalVisible={isIncomeModalVisible} handleIncomeModal={handleIncomeModal} onFinish={onFinish} />
+      <AddExpenses isExpenseModalVisible={isExpenseModalVisible} handleExpenesModal={handleExpenesModal} onFinish={onFinish} />
+
     </div>
   )
 }
