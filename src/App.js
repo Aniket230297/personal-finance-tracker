b363f1764857e14b2b3bd01256d2947071e344ac
Aniket <aniketkule231997@gmail.com>
2024-01-25 13:01:54 +0530

    ff

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
             <Routes>
               <Route path='/' element={<Signup />} />
               <Route path='/dashboard' element={<Dashboard/>} />
+              <Route path='/personal-finance-tracker' element={<Signup />} />
             </Routes>
         </div>
     </BrowserRouter>
